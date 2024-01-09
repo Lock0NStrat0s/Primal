4bf2a4356d6053ff3ebda6b737e0a8cbc3fae568
Mohammad Jabir <mohammadj94@gmail.com>
2024-01-09 12:29:07 -0700

    completed episode 2

---
+++ PrimalEditor/GameProject/NewProject.cs
@@ -0,0 +1,104 @@
+﻿using PrimalEditor.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimalEditor.GameProject
+{
+    [DataContract]
+    public class ProjectTemplate
+    {
+        [DataMember]
+        public string ProjectType { get; set; }
+        [DataMember]
+        public string ProjectFile { get; set; }
+        [DataMember]
+        public List<string> Folders { get; set; }
+        public byte[] Icon { get; set; }
+        public byte[] Screenshot { get; set; }
+        public string IconFilePath { get; set; }
+        public string ScreenshotFilePath { get; set; }
+        public string ProjectFilePath { get; set; }
+    }
+
+    class NewProject : ViewModelBase
+    {
+        // TODO: get the path from the installation location
+        private readonly string _templatePath = @"..\..\..\ProjectTemplates";
+        private string _projectName = "NewProject";
+        public string ProjectName
+        {
+            get => _projectName;
+            set
+            {
+                if (_projectName != value)
+                {
+                    _projectName = value;
+                    OnPropertyChanged(nameof(ProjectName));
+                }
+            }
+        }
+
+        private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\PrimalProject\";
+        public string ProjectPath
+        {
+            get => _projectPath;
+            set
+            {
+                if (_projectPath != value)
+                {
+                    _projectPath = value;
+                    OnPropertyChanged(nameof(ProjectPath));
+                }
+            }
+        }
+
+        private ObservableCollection<ProjectTemplate> _projectTemplates = new ObservableCollection<ProjectTemplate>();
+        public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates { get; }
+
+        public NewProject()
+        {
+            ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projectTemplates);
+            try
+            {
+                var templatesFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templatesFiles.Any());
+                foreach (var file in templatesFiles)
+                {
+                    // Generate templates - once complete, no longer needed
+                    /*var template = new ProjectTemplate()
+                    {
+                        ProjectType = "Empty Project",
+                        ProjectFile = "project.primal",
+                        Folders = new List<string>() { ".Primal", "Content", "GameCode" }
+                    };
+                    
+                    Serializer.ToFile(template, file);*/
+
+                    /*********************************************************************/
+
+                    // Read from template
+                    var template = Serializer.FromFile<ProjectTemplate>(file);
+                    template.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "icon.jpg"));
+                    template.Icon = File.ReadAllBytes(template.IconFilePath);
+                    template.ScreenshotFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "screenshot.jpg"));
+                    template.Screenshot = File.ReadAllBytes(template.ScreenshotFilePath);
+                    template.ProjectFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), template.ProjectFile));
+
+                    _projectTemplates.Add(template);
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                // TODO: log error
+            }
+        }
+    }
+}
